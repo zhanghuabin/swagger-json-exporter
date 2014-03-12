a7fb5c7943b6ae8a6965088aba6abff0a1a6d1f4
Huabin Zhang @Git (v1.7.9) <digitarts@gmail.com>
2014-03-12 19:45:14 +0800

    Add "include-list" option, see help for details

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-swagger-json-exporter
-=====================
+#   Swagger Json Exporter
 
-Swagger API docs Exporter
+This is an utility to use to export the JSON of Swagger API docs from web site to local.
+
+
+##  Prerequisites
+
+*   To run sje.groovy as shell script, you MUST have Groovy environment on local.
+
+    On Mac OSX, Linux and Cygwin, you can use [GVM](http://gvmtool.net) to download and configure any version of you choice.
+
+    Otherwise, you have to download a version of Groovy from [this site](http://groovy.codehaus.org) and configure PATH environment manually.
+
+
+##  Quickstart
+
+*   Run sje.groovy as shell script
+
+    Copy sje.groovy to any location, then run:
+
+        ./sje.groovy -u http://petstore.swagger.wordnik.com/api/api-docs -su http://localhost:8080/apispec/listing -o /tmp/swagger-api-docs -il pet
+
+    You'll get a subset of petstore APIs which includes only /pet path.
+
--- src/main/groovy/sje.groovy
+++ src/main/groovy/sje.groovy
@@ -10,6 +10,7 @@ import groovyx.net.http.HTTPBuilder
 import static java.lang.Math.*
 import static groovyx.gpars.GParsPool.*
 import static groovyx.gpars.agent.Agent.*
+import static org.apache.commons.cli.Option.UNLIMITED_VALUES
 
 
 /**
@@ -37,6 +38,7 @@ def cli = new CliBuilder().with {
     u   longOpt: 'api-docs-url',            "The URL of original swagger api-docs <default: $API_DOCS_URL>",                    args: 1, argName: 'u'
     su  longOpt: 'static-sub-api-docs-url', "The URL of static swagger sub-api-docs URL <default: $STATIC_SUB_API_DOCS_URL>",   args: 1, argName: 'su'
     o   longOpt: 'output-dir',              "The output directory <default: $OUTPUT_DIR>",                                      args: 1, argName: 'o'
+    il  longOpt: 'include-list',            'The include-list <default: null>', args: UNLIMITED_VALUES, valueSeparator: ',', argName:  'arg1,arg2,arg3...'
 
 
     usage = 'sje [options]'
@@ -64,8 +66,9 @@ if (confFile.exists()) {
 }
 
 def apiDocsUrl              = options.u     ?: conf?.apiDocsUrl             ?: API_DOCS_URL
-def staticSubApiDocsUrl     = options.su    ?: conf?.staticSubApiDocsUrl   ?: STATIC_SUB_API_DOCS_URL
+def staticSubApiDocsUrl     = options.su    ?: conf?.staticSubApiDocsUrl    ?: STATIC_SUB_API_DOCS_URL
 def outputDir               = options.o     ?: conf?.outputDir              ?: OUTPUT_DIR
+def includeList             = options.ils   ?: conf?.includeList            ?: null
 
 // print options summary
 def optionsSummary =
@@ -75,6 +78,7 @@ ${''.padRight PAD_LENGTH_1, '-'}
 ${'apiDocsUrl'.padRight PAD_LENGTH_2} = $apiDocsUrl
 ${'staticSubApiDocsUrl'.padRight PAD_LENGTH_2} = $staticSubApiDocsUrl
 ${'outputDir'.padRight PAD_LENGTH_2} = ${(outputDir as File).absolutePath}
+${'includeList'.padRight PAD_LENGTH_2} = ${includeList ? includeList.join(',') : null }
 """
 println optionsSummary
 
@@ -82,45 +86,52 @@ println optionsSummary
 // processing logic
 def apiDocsHttp = [apiDocsUrl] as HTTPBuilder
 def apiDocs = apiDocsHttp.get([:])
-def apiDocs_apis = apiDocs.apis
-def subApiDocs = agent([])
+apiDocs.apis = !includeList ? apiDocs.apis
+        : apiDocs.apis.findAll { api ->
+    def path = api.path
+    includeList.any { path.contains it }
+}
 
-withPool(min(apiDocs_apis.size(), Runtime.runtime.availableProcessors())) {
-    apiDocs_apis.eachParallel { api ->
-        def subApiDocHttp = ["$apiDocsUrl/${api.path}"] as HTTPBuilder
-        def subApiDoc = subApiDocHttp.get([:])
-        subApiDocs << { it << subApiDoc }
-        subApiDocHttp.shutdown()
+if (!apiDocs.apis) {
+    println "<INFO> No entry exported."
+} else {
+
+    def subApiDocs = agent([])
+
+    withPool(min(apiDocs.apis.size(), Runtime.runtime.availableProcessors())) {
+        apiDocs.apis.eachParallel { api ->
+            def subApiDocHttp = ["$apiDocsUrl/${api.path}"] as HTTPBuilder
+            def subApiDoc = subApiDocHttp.get([:])
+            subApiDocs << { it << subApiDoc }
+            subApiDocHttp.shutdown()
+        }
     }
-}
-apiDocsHttp.shutdown()
+    apiDocsHttp.shutdown()
 
-//println new JsonBuilder(apiDocs).toPrettyString()
-//subApiDocs.val.each { println new JsonBuilder(it).toPrettyString() }
+    // update api-docs
+    apiDocs.apis.each { it.path = "$staticSubApiDocsUrl/${it.path}" }
+    def staticApiDocsJson = new JsonBuilder(apiDocs).toPrettyString()
+    // output api-docs
+    def staticApiDocsFile = "$outputDir/api-docs" as File
+    staticApiDocsFile.parentFile.mkdirs() // insure the output dir is existed
+    staticApiDocsFile.withWriter {
+        def writer = it as BufferedWriter
+        writer << "$staticApiDocsJson\n"
+        writer.flush()
+    }
 
-// update api-docs
-apiDocs_apis.each { it.path = "$staticSubApiDocsUrl/${it.path}" }
-def staticApiDocsJson = new JsonBuilder(apiDocs).toPrettyString()
-// output api-docs
-def staticApiDocsFile = "$outputDir/api-docs" as File
-staticApiDocsFile.parentFile.mkdirs() // insure the output dir is existed
-staticApiDocsFile.withWriter {
-    def writer = it as BufferedWriter
-    writer << "$staticApiDocsJson\n"
-    writer.flush()
-}
-
-// sub-apis
-withPool(min(subApiDocs.val.size(), Runtime.runtime.availableProcessors())) {
-    subApiDocs.val.eachParallel { subApiDoc ->
-        def staticSubApiDocJson = new JsonBuilder(subApiDoc).toPrettyString()
-        def resPath = subApiDoc.resourcePath
-        def subApiDocFile = "$outputDir/listing/$resPath" as File
-        subApiDocFile.parentFile.mkdirs()
-        subApiDocFile.withWriter {
-            def writer = it as BufferedWriter
-            writer << "$staticSubApiDocJson\n"
-            writer.flush()
+    // sub-apis
+    withPool(min(subApiDocs.val.size(), Runtime.runtime.availableProcessors())) {
+        subApiDocs.val.eachParallel { subApiDoc ->
+            def staticSubApiDocJson = new JsonBuilder(subApiDoc).toPrettyString()
+            def resPath = subApiDoc.resourcePath
+            def subApiDocFile = "$outputDir/listing/$resPath" as File
+            subApiDocFile.parentFile.mkdirs()
+            subApiDocFile.withWriter {
+                def writer = it as BufferedWriter
+                writer << "$staticSubApiDocJson\n"
+                writer.flush()
+            }
         }
     }
 }
